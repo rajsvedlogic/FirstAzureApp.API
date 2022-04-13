e5709c91ffbad496b3b15a1cd6bb387769b714fa
Raj Sharma <rajs@vedlogic.com>
2022-04-13 10:24:16 +0530

    Add project files.

---
+++ FirstAzureApp.API/Base/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//udf
+using FirstAzureApp.API.Core;
+
+namespace FirstAzureApp.API.Base
+{
+    public abstract class Controller
+    {
+        protected readonly Bootstrap Function;
+        public Controller(Bootstrap wrapper)
+        {
+            Function = wrapper;
+        }
+    }
+}
---
+++ FirstAzureApp.API/Common/Models/CompanyModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstAzureApp.API.Common.Models
+{
+    public class CompanyModel : Base.Model
+    {
+
+        public CompanyModel() : base(Services.CompanyService.EntityLogicalName)
+        {
+        }
+
+        public enum StatusReason
+        {
+            Active,
+            Inactive,
+        }
+
+        public CompanyModel(Entity entity) : base(entity)
+        {}
+
+
+        [Base.Mapping("az_name")]
+        public new string name { get; set; }
+    }
+}
+
